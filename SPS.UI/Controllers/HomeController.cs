195f74162b2040251baf950334d1314c7fb47616
sapana1998 <sapanawabale1998@gmail.com>
2021-09-09 07:31:02 -0700

    Common: sprint1 : #101: Sapana Wabale : home controller add

--- SPS.UI/Controllers/HomeController.cs
+++ SPS.UI/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace SPS.UI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
     }
 }
